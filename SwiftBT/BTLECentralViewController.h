8a76a50b32f68fae60b0a69dad552a7469978bda
smartiothome <smartiothome@icloud.com>
2014-10-11 11:51:50 -0700

    Making changes to create BT RSSI ranger

---
+++ SwiftBT/BTLECentralViewController.h
@@ -0,0 +1,14 @@
+/*
+ 
+ File: LECentralViewController.h
+ 
+ Abstract: Interface to use a CBCentralManager to scan for, and receive 
+ data from, a version of the app in Peripheral Mode
+ 
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface BTLECentralViewController : UIViewController
+
+@end
---
+++ SwiftBT/BTLEPeripheralViewController.h
@@ -0,0 +1,17 @@
+/*
+ 
+ File: LEPeripheralViewController.h
+ 
+ Abstract: Interface to allow the user to enter data that will be 
+ transferred to a version of the app in Central Mode, when it is brought
+ close enough.
+ 
+*/
+
+
+
+#import <UIKit/UIKit.h>
+
+@interface BTLEPeripheralViewController : UIViewController
+
+@end
